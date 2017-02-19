30192cbbc3f4664c6c87700693583a34fbacb4ef
chinjon <Jonathankchin@gmail.com>
2017-02-19 14:40:31 -0500

    added typed js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,12 @@
       $('.carousel.carousel-slider').carousel({
           full_width: true
       });
+
+
+      $(function(){
+      $("#small-typed").typed({
+        strings: ["Web Developer", "Writer", "Photographer", "Web Developer"],
+        typeSpeed: 0
+      });
+  });
   });
