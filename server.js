e67b334122e3e3c576d6fe5196e62bc0d96959a1
chinjon <Jonathankchin@gmail.com>
2017-03-10 18:49:31 -0500

    prepping for heroku deploy and handlebars

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const server = require("http").createServer(app);
+
+var app = express();
+
+var port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: "application/vnd.api+json"}))
+
+app.use(express.static('app/public'))
+
+app.listen(process.env.PORT || port)
