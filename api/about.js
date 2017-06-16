3ae86767a9c3ffa5e32960d04644b149653e78e1
Jonathan <jonathankchin@gmail.com>
2017-06-16 17:31:53 -0400

    typos and added paragraph for about

--- api/about.js
+++ api/about.js
@@ -1,5 +1,7 @@
 const about = [
-    "Hey there, I am Jonathan Chin, a New York-based web developer. I work across the JavaScript technologies, mostly within the confines of the MERN stack."
+    "Hey there, I am Jonathan Chin, a New York-based web developer. I work across JavaScript technologies, mostly within the confines of the MERN stack.",
+
+    "My infatuation for programming derives from a desire for collaboration with the goal to create practical applications."
 ]
 
 module.exports = about;
