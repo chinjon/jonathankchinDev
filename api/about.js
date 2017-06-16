47e017a428c968f54e266e0435a935b21f1d470b
Jonathan <jonathankchin@gmail.com>
2017-06-16 17:47:05 -0400

    edited layout of work cards

--- api/about.js
+++ api/about.js
@@ -2,6 +2,8 @@ const about = [
     "Hey there, I am Jonathan Chin, a New York-based web developer. I work across JavaScript technologies, mostly within the confines of the MERN stack.",
 
     "My infatuation for programming derives from a desire for collaboration with the goal to create practical applications."
-]
+];
+
+const skills = ["HTML5", "CSS", "JavaScript", "jQuery", "NodeJS", "mySQL", "MongoDB", "Functional Programming", "Git", "Version Control", "RESTful APIs", "AJAX", "Responsive Design", "ReactJS",];
 
 module.exports = about;
