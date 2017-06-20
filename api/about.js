cfd2910f4fefbe2f60ce182351abd7948e08ca25
Jonathan <jonathankchin@gmail.com>
2017-06-20 14:27:59 -0400

    typo in about

--- api/about.js
+++ api/about.js
@@ -1,7 +1,7 @@
 const about = [
     "Hey there, I am Jonathan Chin, a New York-based web developer. I work across JavaScript technologies, mostly within the confines of the MERN stack.",
 
-    "My infatuation for programming derives from a desire for collaboration with the goal to create practical applications.",
+    "My infatuation with programming derives from a desire for collaboration with the goal to create practical applications.",
 
     "Currently, I am seeking opporunities, working towards being a better developer everyday, and co-running Cereal + Sounds with my friend Nadine."
 ];
