589b6692eaa5ed279b3ef70aa6f4d9f2a384a756
Jonathan <jonathankchin@gmail.com>
2017-07-06 15:56:13 -0400

    added favicon

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express'),
       routes = require('./routes/routes'),
       exphbs = require('express-handlebars');
 
-// const bodyParser = require('body-parser');
-
 const app = express();
 app.use(express.static('./public'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -11,10 +9,6 @@ app.set('view engine', 'handlebars');
 
 const port = process.env.PORT || 3000;
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.text());
-// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use('/', routes);
 app.listen(port, () => {
     console.log("App is running on PORT: ", port);
