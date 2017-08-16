9cdb7db3b9d32b6572421e50f86511ff67770966
Jonathan <jonathankchin@gmail.com>
2017-08-16 12:45:56 -0400

    updated portfolio - about section, right now section

--- api/about.js
+++ api/about.js
@@ -1,9 +1,9 @@
 const about = [
-    "Hey there, I am Jonathan Chin, a New York-based web developer. I work across JavaScript technologies, mostly within the confines of the MERN stack.",
+    "Hey there, I am Jonathan Chin, a New York-based front end developer. I work with the Web's trifecta, HTML, CSS, and JavaScript.",
 
     "My infatuation with programming derives from a desire for collaboration with the goal to create practical applications.",
 
-    "Currently, I am seeking opporunities, working towards being a better developer everyday, and co-running Cereal + Sounds with my friend Nadine."
+    "Currently, I am a Junior Front End Developer, and co-running Cereal + Sounds with my friend Nadine in my spare time."
 ];
 
 module.exports = about;
--- api/skills.js
+++ api/skills.js
@@ -1,3 +1,3 @@
-const skills = ["HTML5", "CSS", "JavaScript", "jQuery", "NodeJS", "mySQL", "MongoDB", "Functional Programming", "Git", "Version Control", "RESTful APIs", "AJAX", "Responsive Design", "ReactJS", "Ruby", "MeteorJS", "HandlebarsJS", "NPM", "Yarn", "Webpack"];
+const skills = ["HTML5", "CSS", "JavaScript", "jQuery", "NodeJS", "SVG", "Flexbox", "mySQL", "MongoDB", "Functional Programming", "Git", "Version Control", "RESTful APIs", "AJAX", "Responsive Design", "ReactJS", "Ruby", "MeteorJS", "HandlebarsJS", "NPM", "Yarn", "Webpack"];
 
 module.exports = skills;
