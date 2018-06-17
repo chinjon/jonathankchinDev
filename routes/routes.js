d234f1267266d891594c82fb44e61b81541649d9
Jonathan Chin <jonathankchin@gmail.com>
2018-06-17 10:07:22 -0400

    updated routes

--- routes/routes.js
+++ routes/routes.js
@@ -6,25 +6,21 @@ let hbsHelper = exphbs.create({
 })
 
 const home = require('./../api/home');
-const workData = require('./../api/work');
-const about = require('./../api/about');
+
 const likes = require('./../api/likes');
-const skills = require('./../api/skills');
+
 
 routes.get('/', (req, res) => {
     res.render('index', {
-        showWork: false,
         showTitle: true,
         showAbout: false,
         pageTitle: "Full Stack JavaScript Developer",
         home,
-        skills
     });
 });
 
 routes.get('/work', (req, res) => {
     res.render('index', {
-        showWork: true,
         showTitle: false,
         showAbout: false,
         pageTitle: "Work",
@@ -32,17 +28,6 @@ routes.get('/work', (req, res) => {
         
     })
 });
-
-routes.get('/about', (req, res) => {
-    res.render('index', {
-        showWork: false,
-        showTitle: false,
-        showAbout: true,
-        pageTitle: 'About',
-        about,
-        likes,
-    })
-})
 
 routes.get('*', (req, res) => {
     res.render('404', {status: 404})
