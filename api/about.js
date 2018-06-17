84ff5ee639bf3f7b52825aa06d102fd65fec2935
Jonathan Chin <jonathankchin@gmail.com>
2018-06-17 10:04:27 -0400

    deleted about page

--- api/about.js
+++
@@ -1,9 +0,0 @@
-const about = [
-    "Hey there, I am Jonathan Chin, a New York-based front end developer. I work with the Web's trifecta, HTML, CSS, and JavaScript.",
-
-    "My infatuation with programming derives from a desire for collaboration with the goal to create practical applications.",
-
-    "Currently, I am a Junior Front End Developer, and co-running Cereal + Sounds with my friend Nadine in my spare time."
-];
-
-module.exports = about;
